f85a298a9b205ea165c3a55103595dfabe3db068
AnaLinsDev <analinsdev@gmail.com>
2021-07-01 13:14:10 -0300

    alterando errorPage

--- src/pages/error/error-page.jsx
+++ src/pages/error/error-page.jsx
@@ -4,7 +4,7 @@ import './error-page.scss'
 const ErrorPage = () => (
     <div>
         <h1>Sorry, that page doesn't exist !!!</h1>
-        <h1> 404 </h1>
+        <h1>  404  </h1>
     </div>
 );
 
