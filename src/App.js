c45df8783e9616c26bcdbfebd12949ab1d0fdbd3
AnaLinsDev <analinsdev@gmail.com>
2021-07-01 13:01:01 -0300

    mudando nomes das pages pt2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import GamesListPage from './pages/games-list/games-list';
-import ErrorPage from './pages/ErrorPage/error-page';
+import ErrorPage from './pages/error/error-page';
 import GameByIdPage from './pages/game-by-id/game-by-id';
 import Header from './components/header/header';
-import AboutMePage from './pages/AboutMePage/about-me';
+import AboutMePage from './pages/about-me/about-me';
 
 function App() {
   return (
